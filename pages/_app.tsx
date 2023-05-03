7e4caf89006599b9a730cca172ebc89723f045ac
hadi499 <hadi.ygcl@gmail.com>
2023-05-03 20:36:20 +0700

    input

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,6 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Modal actionLabel="Submit" isOpen title="tes modal" />
-
       <Layout>
         <Component {...pageProps} />
       </Layout>
